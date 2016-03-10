7633bec5ceea3a01fcab6ea43041eabb4cd54110
Holly F. Hudson <hollyf@swcp.com>
2016-03-10 19:27:14 +0000

    cleaned up and added comments

--- interaction.js
+++ interaction.js
@@ -1,13 +1,15 @@
 $(document).ready(function(){
     $('button').click(function(){
         var month = $('input[name=month]').val();
-        month = add_leading_zero(month * 8);
+        month = add_leading_zero(month * 8);  // must multiply or all dates are blue
         var day = $('input[name=day]').val();
         day = add_leading_zero(day * 3);
         var year = $('input[name=year]').val();
-        var color = '#' + month + day + year;
-        console.log(color);
-        $('#birthdayColor').css("background-color",color);
+        var color = '#' + month + day + year;  // build the color from the date
+        
+        console.log(color);  // for debugging
+        
+        $('#birthdayColor').css("background-color",color);  // set the color
     });
 });
 
