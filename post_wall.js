369ace5c9951e832f390fed90512e2c68710607e
Holly F. Hudson <hollyf@swcp.com>
2016-04-02 16:13:32 +0000

    put color changing solution code in birthday_color

--- post_wall.js
+++ post_wall.js
@@ -3,12 +3,16 @@ http://i.imgur.com/87kIXSN.jpg
 */
 $(document).ready(function() {
     
-    $('button').click(function(){
+    $('#post').click(function(){
         var to_post = $("#message").val();
         console.log("yo" + to_post + "the rest");
-        element = '<div class="posts"><img src="http://i.imgur.com/87kIXSN.jpg" alt="avatar"><span>  ' + to_post + '  </span></div>';
+        var element = '<div class="posts"><img src="http://i.imgur.com/87kIXSN.jpg" alt="avatar"><span>  ' + to_post + '  </span></div>';
         $('#wall').prepend(element);
     });
-    
+    $('#hundreds').click(function(){
+        for (var i = 0; i < 100; i++) {
+            $('#images').append('<img src="https://catlovers2015.files.wordpress.com/2015/05/90725-colorful-kittens.jpg" alt="colorful kittens">');
+        }
+    });
     
 });
